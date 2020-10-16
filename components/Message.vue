8d80d51025d2754a4c31b1f73c4d445ff0cce689
issei.suzuki <issei.karate@gmail.com>
2020-10-16 14:13:55 +0000

    UI

---
+++ components/Message.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="chat-container">
+   <div class="thumbnail-container">
+      <img src="https://iseblog.xyz/wp-content/uploads/2020/10/sigmund-OV44gxH71DU-unsplash.jpg" />
+   </div>
+   <div class="message-container">
+     <div class="user-name">issei1111</div>
+     <div class="message">Hello!</div>
+   </div>
+ </div>
+</template>
+
+<style scoped>
+.chat-container {
+ display: flex;
+ padding: 8px;
+}
+
+.thumbnail-container {
+ margin-right: 16px;
+}
+
+.thumbnail-container img {
+ width: 50px;
+ height: 50px;
+ border-radius: 10px;
+}
+
+.user-name {
+ font-weight: bold;
+}
+</style>
